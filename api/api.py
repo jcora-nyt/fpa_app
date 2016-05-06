f452d3653aa81d77ce36d3c111d8237a4b463dc0
Joe Cora <joseph.cora@nytimes.com>
2016-05-06 16:31:25 -0400

    API returns live data for routes and route detail endpoints

--- api/api.py
+++ api/api.py
@@ -1,40 +1,105 @@
 from flask import request, url_for
 from flask.ext.api import FlaskAPI, status, exceptions
+from accessControl import crossdomain
+import mysql.connector
 
+# Setup Flask API and use custom JSON encoder for decimal issue
 app = FlaskAPI(__name__)
 
-@app.route("/routes/<int:route_id>/", methods=['GET'])
-def route_detail(route_id):
-    response = {
-    	"meta": {
-    		"num_records": 0,
-    		"route_id": route_id
-    	},
-    	"data": {
-	    	"customers": [
-				{
-					"name": "",
-					"address": "",
-					"city": "",
-					"state": "",
-					"zip": "",
-					"geo": {
-						"lat": 41.52,
-						"lng": -71.61
-					},
-					"escalation": {
-						"type": "STANDARD",
-						"level": 2,
-						"status": "OPEN",
-						"product": "DS",
-						"complaint": "WP"
-					}
-				}
-			]
+@app.route("/routes/", methods=['GET', 'OPTIONS'])
+@crossdomain(origin='*')
+def routes():
+	# Create DB connection
+	cnx = mysql.connector.connect(user='root', password='root', host='localhost', database='cis_delivery')
+
+	# Create DB cursor
+	q_cursor = cnx.cursor(buffered=True)
+
+	# Generate and perform query on DB to get routes
+	query = ('SELECT id, name FROM route')
+
+	q_cursor.execute(query)
+
+	# Loop through the routes and populate each route
+	routes = []
+
+	for (route_id, name) in q_cursor:
+		# Add route information and add to routes list
+		route = {
+			"id": route_id,
+			"name": name
+		}
+
+		routes.append(route)
+
+	# Cleanup DB stuff
+	q_cursor.close()
+	cnx.close()
+
+	# Process response and return
+	response = {
+		"meta": {
+			"num_records": len(routes)
+		},
+		"data": {
+			"routes": routes
 		}
 	}
 
-    return response
+	return response
+
+@app.route("/routes/<int:route_id>/", methods=['GET', 'OPTIONS'])
+@crossdomain(origin='*')
+def route_detail(route_id):
+	# Create DB connection
+	cnx = mysql.connector.connect(user='root', password='root', host='localhost', database='cis_delivery')
+
+	# Create DB cursor
+	q_cursor = cnx.cursor(buffered=True)
+
+	# Generate and perform query on DB to get routes
+	query = ('SELECT cs.id, name, address_line1, city, state, zip, phone_home, lat, lng FROM cis_subscriber cs INNER JOIN cis_subscriber_route csr ON cs.id = csr.cis_subscriber_id WHERE route_id = ' + str(route_id))
+
+	q_cursor.execute(query)
+
+	# Loop through the customers and populate each customer
+	customers = []
+
+	for (customer_id, name, address, city, state, zip, phone, lat, lng) in q_cursor:
+		# Add customer information and add to customers list
+		customer = {
+			"id": customer_id,
+			"name": name,
+			"address": address,
+			"city": city,
+			"state": state,
+			"zip": zip,
+			"phone_home": phone,
+			"geo": {
+				"lat": str(lat),
+				"lng": str(lng)
+			},
+			"escalation": None
+		}
+
+		customers.append(customer)
+
+	# Cleanup DB stuff
+	q_cursor.close()
+	cnx.close()
+
+	# Process response and return
+	response = {
+		"meta": {
+			"num_records": len(customers),
+			"route_id": route_id
+		},
+		"data": {
+			"customers": customers
+		}
+	}
+
+	return response
 
 if __name__ == "__main__":
     app.run('10.51.236.201', debug=True)
