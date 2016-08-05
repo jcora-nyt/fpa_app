e9cd173ff2cc841dc6a159e62252f9ea2dda39cc
Joe Cora <joseph.cora@nytimes.com>
2016-08-05 14:46:58 -0400

    More something

--- test.py
+++ test.py
@@ -1,1 +1,12 @@
-print "Hello, World!?"
+from flask import request, url_for
+from flask.ext.api import FlaskAPI, status, exceptions
+
+# Note: We don't need to call run() since our application is embedded within
+# the App Engine WSGI application server.
+
+
+# Setup Flask API and use custom JSON encoder for decimal issue
+app = FlaskAPI(__name__)
+app.config['DEBUG'] = True
+
+print "Here I am, world!"
